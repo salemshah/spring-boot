244fca3d10eda961bb13fd4ce5ba6e6665ff503b
salemshah <salemshah686@gmail.com>
2020-04-10 03:59:30 +0200

    display error message if the data is not valid

--- src/main/java/salem/shah/ppmtool/web/ProjectController.java
+++ src/main/java/salem/shah/ppmtool/web/ProjectController.java
@@ -3,9 +3,16 @@ package salem.shah.ppmtool.web;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.BindingResult;
+import org.springframework.validation.FieldError;
 import org.springframework.web.bind.annotation.*;
 import salem.shah.ppmtool.domain.Project;
 import salem.shah.ppmtool.services.ProjectService;
+
+import javax.validation.Valid;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
 @RestController
 @RequestMapping("/api/project")
@@ -15,8 +22,24 @@ public class ProjectController {
     private ProjectService projectService;
 
     @PostMapping("")
-    public ResponseEntity<Project> createNewProject(@RequestBody Project project){
+    public ResponseEntity<?> createNewProject(@Valid @RequestBody Project project, BindingResult result){
+
+        // This will create a message for bad request
+        if(result.hasErrors()){
+            Map<String, String> errorMap = new HashMap<>();
+            for(FieldError error: result.getFieldErrors()){
+                errorMap.put(error.getField(), error.getDefaultMessage());
+            }
+
+            // this will return the messages that we wrote in Project.java file for every filed.
+            return new ResponseEntity<>(errorMap, HttpStatus.BAD_REQUEST);
+            //return new ResponseEntity<Map<String, String>>(errorMap, HttpStatus.BAD_REQUEST);
+
+            //this will return a object list with mor information
+            //return new ResponseEntity<List<FieldError>>(result.getFieldErrors(), HttpStatus.BAD_REQUEST);
+        }
         Project project1 = projectService.saveOrUpdateProject(project);
-        return new ResponseEntity<Project>(project, HttpStatus.CREATED);
+        return new ResponseEntity<>(project, HttpStatus.CREATED);
+        //return new ResponseEntity<Project>(project, HttpStatus.CREATED);
     }
 }
